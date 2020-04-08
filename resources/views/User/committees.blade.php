2e25dfffd0b538394e7a7e3fdefbfce3688d881e
Ism-10 <esmagdy10@gmail.com>
2020-04-08 03:03:00 +0200

    edited content of committees.blade.php & committees.scss

---
+++ resources/views/User/committees.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../sass/user/committees.scss">
+    <script src="https://use.fontawesome.com/4e185a11e7.js"></script>
+    <title>Our Committees</title>
+</head>
+<body>
+  <h1>Our Committees</h1>
+
+<div class="commet">
+
+
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">PR</h5>
+      <p class="card-text">1- Responsible for providing media sponsors for covering Pirates events. <br>
+        2- Responsible for dealing with any public figures and
+        speakers. <br>
+        3- Representing Pirates during any offline launch & Hosting
+        Pirates events. </p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Advertising</h5>
+      <p class="card-text">1- Designing all the publications (Roll ups, Flyers, Banner,
+        ....etc). <br>
+        2- Creating visual content for online campaigns. <br>
+        3- Responsible for shooting, implementing any videos and
+        photography. </p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">FR</h5>
+      <p class="card-text"> 1- Responsible for making Pirates’ profile and benefits package. <br>
+        2- Responsible for getting all fund needed for all our activities. <br>
+        3- Dealing with companies and making relationships with
+        them. </p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Logistics</h5>
+      <p class="card-text">1- Responsible for booth design, materials and cost analysis. <br>
+        2- Responsible for printing. <br>
+        3- Responsible for decoration, reservations and reception in
+        all events
+         </p>
+    
+  </div>
+    
+</div>
+
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">IT</h5>
+      <p class="card-text">1- Responsible for Creating and developing Pirates website. <br>
+        2- Responsible for making online registration forms. <br>
+        3- Responsible for web data base structure. </p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Marketing</h5>
+      <p class="card-text">1- Responsible for managing social media. <br>
+        2- preparing and executing social media campaign. <br>
+        3- Responsible for online coverage for our on-ground events.</p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">HR</h5>
+      <p class="card-text">1- Developing and implementing punishments and awarding system. <br>
+        2- Evaluating all committees and members. <br>
+        3- Recruiting and developing the selection phases in Pirates.
+         </p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">QM</h5>
+      <p class="card-text">1- Setting the standards of Pirates and tracking the
+        processes of producing the outputs. <br>
+        2- Checking all the final output to make sure that
+        everything satisfy the standards.<br>
+        3- The research, documentation and development of the
+        season. </p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">AC Technical</h5>
+      <p class="card-text">1- Guiding and following up with the participants. <br>
+        2- Preparing of the technical workshops and their materials. <br></p>
+    
+  </div>
+    
+</div>
+
+<div class="card mb-3">
+    <img src="IMG_20170831_122933_627.JPG" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">AC Non-Technical</h5>
+      <p class="card-text">1- Guiding and following up with the participants. <br>
+        2- Preparing of the non-technical workshops and their
+        materials.</p>
+    
+  </div>
+    
+</div>
+
+
+
+
+</div>
+
+
+
+
+</body>
+</html>
